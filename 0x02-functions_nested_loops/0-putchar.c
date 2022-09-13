84817407bf251ef9ae2db3f3ccb986685cacf2d4
Sammywens <nwene.samuel@gmail.com>
2022-09-13 14:14:50 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,37 @@
+#include "Sammywens.h"
+
+/**
+
+ * main - Entry point
+
+ *
+
+ * Return: Always 0 (Success)
+
+ */
+
+int main(void)
+  
+{
+  
+  char *sh = "Sammywens";
+  
+
+  
+  while (*sh)
+    
+    {
+      
+      _putchar(*sh);
+      
+      sh++;
+      
+    }
+  
+  _putchar('\n');
+  
+
+  
+  return (0);
+  
+}
