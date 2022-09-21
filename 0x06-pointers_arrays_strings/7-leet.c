2048f91242c018a8c26ca918b246be2c06aab98b
Sammywens <111000998+Sammywens@users.noreply.github.com>
2022-09-21 21:56:07 +0100

    Update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,46 +1,28 @@
 #include "main.h"
 
 /**
- * leet - leet encoder
- * @str: string to be encoded
- *
- * Return: address of the encoded string
+ * leet - Entry point
+ * ONE if, TWO loops only...
+ * @n: input
+ * Return: Always 0 (Success)
  */
-
-char *leet(char *str)
+char *leet(char *n)
 {
-	int i = 0;
+int i, x;
+int find[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+int replacer[] = {'4', '3', '0', '7', '1'};
 
-	while (str[i] != '\0')
-	{
-		str[i] = transform(str[i]);
-		i++;
-	}
-	return (str);
+for (i = 0; n[i] != 0; i++)
+{
+for (x = 0; x <= 9; x++)
+{
+if (n[i] == find[x])
+{
+n[i] = replacer[x / 2];
+x = 9;
+}
+}
 }
 
-/**
- * transform - helper function to map a letter with it's leet encoding
- * @x: char to be encoded
- *
- * Return: the encoded char
- */
-
-char transform(char x)
-{
-	char mapping_low[8] = {'o', 'l', '\0', 'e', 'a', '\0', '\0', 't'};
-	char mapping_upper[8] = {'O', 'L', '\0', 'E', 'A', '\0', '\0', 'T'};
-	int i = 0;
-	char replacement = x;
-
-	while (i < 8)
-	{
-		if (x == mapping_low[i] || x == mapping_upper[i])
-		{
-			replacement = i + '0';
-			break;
-		}
-		i++;
-	}
-	return (replacement);
+return (n);
 }
